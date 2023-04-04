ed7ef78cb89eff019d788f2a0cafa44f33cb6845
999MeshDi666 <havkpzz@gmail.com>
2023-04-04 12:29:52 +0600

    change font styles

--- src/components/code-panel/code-panel-monitor/CodePanelMonitor.jsx
+++ src/components/code-panel/code-panel-monitor/CodePanelMonitor.jsx
@@ -2,18 +2,16 @@ import TextArea from "./TextArea";
 
 const CodePanelMonitor = ({ level, description }) => {
   const parent = level.parentStyles ? "" : ".board {\n  display: flex; \n}";
-  const child = level.parentStyles ? ".board {\n display: flex;" : `.${level.childName} {`
+  const child = level.parentStyles
+    ? ".board {\n display: flex;"
+    : `.${level.childName} {`;
   return (
     <div className="monitor code-panel-monitor">
       <p className="line-numbers"> 1 2 3 4 5 6 7 8 9 10 11 12</p>
       <div className="css-content">
-        <p className="css-code text-content">
-          {parent}
-        </p>
-        <p className="css-code text-content">
-          {child}
-        </p>
-        <TextArea description={description}/>
+        <p className="css-code text-content">{parent}</p>
+        <p className="css-code text-content">{child}</p>
+        <TextArea description={description} />
         <p className="text-content">{"}"}</p>
       </div>
     </div>
